c3f5a307485c4ddb7892c8189b5e709b3c897922
Ebin Paul <ebinpaul@outlook.com>
2020-12-31 09:35:08 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,10 +1,11 @@
-# Given an array of integers nums and an integer target, 
-# return indices of the two numbers such that they add up to target.
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice."""
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Difficulty: Easy
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
