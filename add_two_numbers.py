35865bf41627aef30d31cc55e39a1cd0c9eef3bb
Ebin Paul <ebinpaul@outlook.com>
2021-01-09 19:59:12 +0530

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -6,10 +6,10 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 # Difficulty: Medium
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
