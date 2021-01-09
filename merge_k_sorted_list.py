8efcd139a25c4bbd2b6b5c7899b9bd3b7f260894
Ebin Paul <ebinpaul@outlook.com>
2021-01-09 19:59:16 +0530

    Update merge_k_sorted_list.py

--- merge_k_sorted_list.py
+++ merge_k_sorted_list.py
@@ -5,10 +5,11 @@ Merge all the linked-lists into one sorted linked-list and return it."""
 # Difficulty: Hard
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         temp = []
