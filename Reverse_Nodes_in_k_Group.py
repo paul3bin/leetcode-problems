2d358f134fdaf056c1d7a53f0251d5f6f95c5ff2
Ebin Paul <ebinpaul@outlook.com>
2021-03-20 17:34:21 +0530

    Update Reverse_Nodes_in_k_Group.py

--- Reverse_Nodes_in_k_Group.py
+++ Reverse_Nodes_in_k_Group.py
@@ -1,15 +1,17 @@
 """Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
-
 k is a positive integer and is less than or equal to the length of the linked list. 
 If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is."""
 
 # Difficulty: Hard
 
 # Definition for singly-linked list.
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
@@ -21,14 +23,14 @@ class Solution:
             while temp:
                 llist.append(temp.val)
                 temp = temp.next
-            if k<=len(llist):
+            if k <= len(llist):
                 front_index, rear_index = 0, 0
                 for i in range(len(llist)+1):
                     rear_index = i
-                    if i%k==0:
+                    if i % k == 0:
                         llist[front_index:rear_index] = llist[front_index:rear_index][::-1]
                         front_index = rear_index
-                
+
                 tempHead = ListNode(0)
                 result = [tempHead]
                 for value in llist:
